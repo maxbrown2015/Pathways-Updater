8aba4a86487506f4db389985ce10b914c25521e8
maxbrown2015 <maxwellbrown2015@gmail.com>
2018-08-08 04:20:23 -0400

    adding top-level parent to choose what type of catalog

---
+++ src/js/ChooseCatalogInterface.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import '../css/ChooseCatalogInterface.css'
+import CourseCatalog from './CourseCatalog'
+import PathwaysCatalog from './PathwaysCatalog'
+
+class ChooseCatalogInterface extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {catalogSelected: ''};
+    t
+  }
+
+  handleChoice(event) {
+    let choice = '';
+    if (choice === 'course') {
+      this.setState({catalogSelected: 'course'})
+    }
+    if (choice === 'pathway') {
+      this.setState({catalogSelected: 'pathway'})
+    }
+  }
+
+  render() {
+    if (this.state.catalogSelected === '') {
+      return <div>PICK A CATALOGUE</div>
+    }
+    if (this.state.catalogSelected === 'course') {
+      return <CourseCatalog data={this.props.courses} pathwayObj={this.props.pathways}/>
+    }
+    if (this.state.catalogSelected === 'pathway') {
+      return <PathwaysCatalog pathwayObj={this.props.pathways}/>
+    }
+  }
+  
+}
+
+export default ChooseCatalogInterface;
